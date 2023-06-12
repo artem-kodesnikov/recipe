3c2458e21c2eaf7105448f0250194a7c02a2a8d4
Artem Kodesnikov <artiguard@gmail.com>
2023-06-12 16:26:58 +0300

    add gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
-import { RecipePage } from './components/RecipePage/RecipePage';
 import { PersistGate } from 'redux-persist/integration/react'
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "recipe/:id",
-    element: <RecipePage />,
-  },
-]);
+import { router } from './route/route';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
